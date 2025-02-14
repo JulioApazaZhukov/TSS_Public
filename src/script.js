5f2c632bda9b0d2004cee2e99cda620179201b47
Julio Apaza <79545504+JulioApazaZhukov@users.noreply.github.com>
2025-02-14 18:04:38 -0500

    02-14-2025

--- src/script.js
+++ src/script.js
@@ -17,3 +17,34 @@ function updateCandle(open, close, high, low) {
 
 // Example usage: updateCandle(open, close, high, low)
 updateCandle(100, 110, 115, 95);
+
+function getFullscreenElement() {
+    return document.fullscreenElement 
+    || document.webkitFullscreenElement 
+    || document.mozFullScreenElement 
+    || document.msFullscreenElement;
+}
+
+function toggleFullscreen() {
+    if (getFullscreenElement()) {
+        document.exitFullscreen().catch((e) => {
+            console.log("Exit fullscreen error");
+        });
+    } else {
+        document.documentElement.requestFullscreen().catch((e) => {
+            console.log("Fullscreen error");
+        });
+    }
+}
+
+document.addEventListener("dblclick", () => {
+    toggleFullscreen();
+});
+
+document.addEventListener("fullscreenchange", () => {
+    if (getFullscreenElement()) {
+        console.log("Entered fullscreen");
+    } else {
+        console.log("Exited fullscreen");
+    }
+});
